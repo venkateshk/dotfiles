68f7d0b456a0be591cad248681b33abf16309621
Venkatesh Kavuluri <venkateshk@users.noreply.github.com>
2015-01-07 13:59:05 -0800

    Update fpti.js

--- fpti.js
+++ fpti.js
@@ -1,8 +1,12 @@
-var ad_url = 'adfarm.mediaplex.com/ad/ck/15978-198668-1039-4?mpt=1736108736&mpcr=&mpcrset=&mpcr=46583331&mpcrset=root';
+//var ad_url = 'adfarm.mediaplex.com/ad/ck/15978-198668-1039-4?mpt=1736108736&mpcr=&mpcrset=&mpcr=46583331&mpcrset=root';
 //var ad_url = '<mpck/>';
 alert("one");
 var x = document.referrer;
 alert(x);
+var pat = /\d+\-\d+\-\d+\-\d+/; //define pattern to match placement id 
+var rot_id = x.match(pat); //extract placement id from mpck
+alert(rot_id);
+var ad_url = 'adfarm.mediaplex.com/ad/ck/'+rot_id;
 var tdate = new Date();
 var tepochtime = Math.round(tdate.getTime()/1000.0);
 var toffset = tdate.getTimezoneOffset();
