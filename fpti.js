600443ca4fde51cb2309a9d12dfd29bd019b4694
Venkatesh Kavuluri <venkateshk@users.noreply.github.com>
2015-01-06 16:22:58 -0800

    Update fpti.js

--- fpti.js
+++ fpti.js
@@ -4,24 +4,6 @@ var tepochtime = Math.round(tdate.getTime()/1000.0);
 var toffset = tdate.getTimezoneOffset();
 var fpti = document.createElement("script");
 var proto = window.location.protocol;
-
-function getCookie(name) {
- var re = new RegExp(name + "=([^;]+)");
- var value = re.exec(document.cookie);
- return (value != null) ? unescape(value[1]) : null;
-}
-
-(function() {    	
-var ppb = document.createElement("img");
-var mplx_svid=getCookie("svid");
-//alert(mplx_svid);
-
-var pat = /\d+\-\d+\-\d+\-\d+/; //define pattern to match placement id 
-var rot_id = ad_url.match(pat); //extract placement id from mpck 
-var pgrp = escape("main:mktg:display:mplx:" + rot_id);
-var page = escape("main:mktg:display:mplx:" + rot_id + ":::" + rot_id);
-ppb.src = 'https://t.paypal.com/ts?v=0.1&t=' + tepochtime + '&e=mplx_im&pgrp=' + pgrp + '&page=' + page + '&mchn=dp&s=ci&spid=' + escape(mplx_svid) + "&g=" + toffset;
-//ppb.src = 'https://t.paypal.com/ts?v=0.1&t=' + tepochtime + '&e=mplx_im&pgrp=' + pgrp + '&page=' + page;
-ppb.border = "0";
-document.documentElement.firstChild.appendChild(ppb);
-})();
+var src_url = ((proto.match('https:')) ? '//secure.img-cdn.mediaplex.com' : '//img-cdn.mediaplex.com');
+fpti.src = proto + src_url + '/0/3484/130154/paypal-fpti.js';
+document.documentElement.firstChild.appendChild(fpti);
