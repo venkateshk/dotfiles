f2baa6e0c39e7b73bd40349d98c93269055ff163
Venkatesh Kavuluri <venkateshk@users.noreply.github.com>
2015-01-08 12:08:23 -0800

    Update fpti.js

--- fpti.js
+++ fpti.js
@@ -4,7 +4,6 @@ function getCookie(name) {
  return (value != null) ? unescape(value[1]) : null;
 }
 (function() {    
-alert("inner-1");
 var ad_url;
 var tdate = new Date();
 var tepochtime = Math.round(tdate.getTime()/1000.0);
@@ -13,12 +12,10 @@ var ppb = document.createElement("img");
 var mplx_svid=getCookie("svid");
 var pat = /\d+\-\d+\-\d+\-\d+/; //define pattern to match placement id 
 var scripts = document.getElementsByTagName('script');
-alert(scripts.length);
 for (var i = 0; i < scripts.length; i++) { 
 	var scriptSrc = scripts[i].src; 
 	if ( scriptSrc.indexOf("adfarm.mediaplex.com") > -1 && scriptSrc.match(pat)) { 
         ad_url = scriptSrc;
-        alert(ad_url);
         break;
     }
 }
