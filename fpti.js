3cba37d0247083dded007d03554f4401ec3bbdbf
vkavuluri <vkavuluri@paypal.com>
2015-01-08 11:56:50 -0800

    Refactor for image type

--- fpti.js
+++ fpti.js
@@ -1,27 +1,27 @@
-//var ad_url = 'adfarm.mediaplex.com/ad/ck/15978-198668-1039-4?mpt=1736108736&mpcr=&mpcrset=&mpcr=46583331&mpcrset=root';
-//var ad_url = '<mpck/>';
-//var x = document.referrer;
-//avar ad_url = 'adfarm.mediaplex.com/ad/ck/'+rot_id;
-var tdate = new Date();
-var tepochtime = Math.round(tdate.getTime()/1000.0);
-var toffset = tdate.getTimezoneOffset();
-var fpti = document.createElement("script");
-//var proto = window.location.protocol;
-//var src_url = ((proto.match('https:')) ? '//secure.img-cdn.mediaplex.com' : '//img-cdn.mediaplex.com');
-//fpti.src = proto + src_url + '/0/3484/130154/paypal-fpti.js';
-//document.documentElement.firstChild.appendChild(fpti);
 function getCookie(name) {
  var re = new RegExp(name + "=([^;]+)");
  var value = re.exec(document.cookie);
  return (value != null) ? unescape(value[1]) : null;
 }
 (function() {    
+var ad_url;
+var tdate = new Date();
+var tepochtime = Math.round(tdate.getTime()/1000.0);
+var toffset = tdate.getTimezoneOffset();	
 var ppb = document.createElement("img");
 var mplx_svid=getCookie("svid");
 var pat = /\d+\-\d+\-\d+\-\d+/; //define pattern to match placement id 
-//var rot_id = ad_url.match(pat); //extract placement id from mpck 
-var pgrp = escape("main:mktg:display:mplx:" + ppCampId);
-var page = escape("main:mktg:display:mplx:" + ppCampId + ":::" + ppCampId);
+var scripts = document.getElementsByTagName('script');
+for (var i = 0; i < scripts.length; i++) { 
+	var scriptSrc = scripts[i].src; 
+	if ( scriptSrc.indexOf("adfarm.mediaplex.com") > -1 && !scriptSrc.match(pat)) { 
+        ad_url = scriptSrc;
+        break;
+    }
+}
+var rot_id = ad_url.match(pat);
+var pgrp = escape("main:mktg:display:mplx:" + rot_id);
+var page = escape("main:mktg:display:mplx:" + rot_id + ":::" + rot_id);
 ppb.src = 'https://t.paypal.com/ts?v=0.1&t=' + tepochtime + '&e=mplx_im&pgrp=' + pgrp + '&page=' + page + '&mchn=dp&s=ci&spid=' + escape(mplx_svid) + "&g=" + toffset;
 ppb.border = "0";
 document.documentElement.firstChild.appendChild(ppb);
