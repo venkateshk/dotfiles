ad7d0b92e7c6baa43d8a5b52f047d6c92d818cf9
Venkatesh Kavuluri <venkateshk@users.noreply.github.com>
2015-01-13 17:34:22 -0800

    Update fpti.js

--- fpti.js
+++ fpti.js
@@ -1,5 +1,6 @@
 function getCookie(name) {
  var re = new RegExp(name + "=([^;]+)");
+ alert(document.cookie);
  var value = re.exec(document.cookie);
  return (value != null) ? unescape(value[1]) : null;
 }
